a281fb39f8187f66f159b631efeb2a37f70068ed
SebastianParrado <47955595+SebastianParrado@users.noreply.github.com>
2019-05-17 22:45:55 -0500

    Update counter_tb.vhd

--- counter_tb.vhd
+++ counter_tb.vhd
@@ -7,43 +7,53 @@ ENTITY counter_tb IS
 END ENTITY counter_tb;
 -------------------------------------------------------
 ARCHITECTURE testbench OF counter_tb IS
-	SIGNAL 	clk_tb	:	STD_LOGIC := '0';
-	SIGNAL	rst_tb	:	STD_LOGIC := '1';
-	SIGNAL	ena_tb	:	STD_LOGIC := '0';
+	SIGNAL 	clk_tb		:	STD_LOGIC := '0';
+	SIGNAL	rst_tb		:	STD_LOGIC;
+	SIGNAL	ena_tb		:	STD_LOGIC := '0';
+	SIGNAL 	count_ini_tb	:	STD_LOGIC_VECTOR(7 DOWNTO 0) := "ZZZZZZZZ";
+	SIGNAL	count_fin_tb	:	STD_LOGIC_VECTOR(7 DOWNTO 0) := "ZZZZZZZZ";
 	SIGNAL	q_tb		:	STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL	int_T_tb	:	STD_LOGIC;
 -------------------------------------------------------
-BEGIN
-	DUT1: ENTITY work.counterGates
-	PORT MAP (	clk_T	=> clk_tb,
-					rst_T	=> rst_tb,
-					ena_T	=> ena_tb,
-					q_T	=>	q_tb);
-					
-	DUT2: ENTITY work.counter
-	PORT MAP (	clk	=> clk_tb,
-					rst	=> rst_tb,
-					ena	=> ena_tb,
-					q		=>	q_tb);
+BEGIN		
+	DUT: ENTITY work.counter
+	PORT MAP (	clk			=> clk_tb,
+			rst			=> rst_tb,
+			ena			=> ena_tb,
+			count_ini	=> count_ini_tb,
+			count_fin	=> count_fin_tb,
+			q				=>	q_tb,
+			int_T			=> int_T_tb);
 	-- CLOCK GENERATION
-	clk_tb <= NOT(clk_tb) AFTER 50 ns;
+	clk_tb <= NOT(clk_tb) AFTER 20 ns;
 	
 	-- TEST VECTORS
-	rst_tb <= '0' AFTER 200 ns;
+	count_ini_tb <= "XXXXXXXX" AFTER 200 ns,
+			"ZZZZZZZZ" AFTER 11 us,
+			"00010010"	AFTER 22 us;
+	count_fin_tb <= "10101100" AFTER 200 ns,
+			"00110100" AFTER 11 us,
+			"ZZZZZZZZ" AFTER 22 us;
+signal_generation: PROCESS
+BEGIN
+	-- TEST VECTOR 1
+	ena_tb <= '0';
+	rst_tb <= '1';
+	WAIT FOR 200 ns;
 	
-	ena_tb <= '1' AFTER 200 ns;
---	signal_generation: PROCESS
---	BEGIN
---	-- TEST VECTOR 1
---	rst_tb <= '1';
---	ena_tb <= '0';
---	WAIT FOR 200 ns;
---	-- TEST VECTOR 2
---	rst_tb <= '0';
---	ena_tb <= '1';
---	WAIT FOR 1800 ns;
---	-- TEST VECTOR 3
---	rst_tb <= '1';
---	ena_tb <= '0';
---	WAIT FOR 200 ns;
---END PROCESS;
-END ARCHITECTURE;
+	-- TEST VECTOR 2
+	ena_tb <= '1';
+	rst_tb <= '0';
+	WAIT FOR 11 us;
+	
+	-- TEST VECTOR 3
+	ena_tb <= '0';
+	rst_tb <= '1';
+	WAIT FOR 200 ns;
+
+	-- TEST VECTOR 4
+	ena_tb <= '1';
+	rst_tb <= '0';
+	WAIT FOR 11 us;
+END PROCESS;
+END ARCHITECTURE testbecnh;
